a6ee655606590ff97c12baaa6a156a6f1fa70321
Анна Рудницкая <anjarudnitckaia@gmail.com>
2022-03-20 02:22:26 +0300

    ...

--- src/main/kotlin/lab1/main.kt
+++ src/main/kotlin/lab1/main.kt
@@ -1,7 +1,5 @@
 package lab1
 
-package lab1
-
 data class Book(val number: String, val name: String, val authors: String) {}
 
 fun parserBooks(books: String): List<Book>? {
@@ -19,15 +17,25 @@ fun parserBooks(books: String): List<Book>? {
     return listBooks
 }
 
+fun oldestBook(){}
 
+fun youngestBook(){}
+
+fun longestNameBook(){}
+
+fun shortestNameBook(){}
 
 fun main() {
     val books = "1. Отцы и дети // И.С.Тургенев // 1862\n"+
             "2. Горе от ума // А.С.Грибоедов // 1825\n"+
             "3. Война и мир // Л.Н.Толстой // 1867\n"+
-            "4. Евгений Онегин // А.С.Пушкин // 1833\n"
+            "4. Евгений Онегин // А.С.Пушкин // 1833\n".trimIndent()
     val listBooks: List<Book>? = parserBooks(books)
     if (listBooks !=null) {
+        println("The oldest book is: ${oldestBook()}")
+        println("The youngest book is: ${oldestBook()}")
+        println("The longest name of the book is: ${oldestBook()}")
+        println("The shortest name of the book is: ${oldestBook()}")
 
     }
 
