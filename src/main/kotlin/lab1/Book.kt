c47c8f1fea648cb62db2c38974b784894fe267d0
Анна Рудницкая <anjarudnitckaia@gmail.com>
2022-03-20 03:03:03 +0300

    ..

---
+++ src/main/kotlin/lab1/Book.kt
@@ -0,0 +1,23 @@
+package lab1
+
+data class Book(val number: Int, val name: String, val authors: String, val yearBook: Int) {}
+
+fun parserBooks(books: String): List<Book>? {
+    if (books.isEmpty()) { return null }
+    val splitString = books.split('\n')
+    val listBooks = mutableListOf<Book>()
+    for (str in splitString) {
+        val number = books.substringBefore('.').toInt()
+        val name = books.substringBeforeLast('.').substringAfter("//")
+        val author = books.substringBeforeLast("//").substringAfter("//")
+        val yearBook = books.substringAfterLast("//").toInt()
+        val fullBook = Book(number, name, author, yearBook)
+        listBooks.add(fullBook)
+    }
+    return listBooks
+}
+
+fun oldestBook(books: List<Book>): Book? { return books.minByOrNull { it.yearBook }}
+fun youngestBook(books: List<Book>): Book? { return books.maxByOrNull { it.yearBook }}
+fun longestNameBook(books: List<Book>): Book? { return books.maxByOrNull { it.name.length }}
+fun shortestNameBook(books: List<Book>): Book? { return books.minByOrNull { it.name.length }}
--- src/main/kotlin/lab1/main.kt
+++ src/main/kotlin/lab1/main.kt
@@ -1,30 +1,5 @@
 package lab1
 
-data class Book(val number: String, val name: String, val authors: String, val yearBook: String) {}
-
-fun parserBooks(books: String): List<Book>? {
-    if (books.isEmpty()) { return null }
-    val splitString = books.split('\n')
-    val listBooks = mutableListOf<Book>()
-    for (str in splitString) {
-        val number = books.substringBefore('.')
-        val name = books.substringBeforeLast('.').substringAfter("//")
-        val author = books.substringBeforeLast("//").substringAfter("//")
-        val yearBook = books.substringAfterLast("//")
-        val strBook = Book(name, author, yearBook)
-        listBooks.add(strBook)
-    }
-    return listBooks
-}
-
-fun oldestBook(books: List<Book>): Book? { return books.minByOrNull { it.yearBook }}
-
-fun youngestBook(books: List<Book>): Book? { return books.maxByOrNull { it.yearBook }}
-
-fun longestNameBook(books: List<Book>): Book? { return books.maxByOrNull { it.name.length }}
-
-fun shortestNameBook(books: List<Book>): Book? { return books.minByOrNull { it.name.length }}
-
 fun main() {
     val books = "1. Отцы и дети // И.С.Тургенев // 1862\n"+
             "2. Горе от ума // А.С.Грибоедов // 1825\n"+
@@ -36,7 +11,8 @@ fun main() {
         println("The youngest book was published: ${oldestBook(listBooks)}")
         println("The longest name of the book is: ${oldestBook(listBooks)}")
         println("The shortest name of the book is: ${oldestBook(listBooks)}")
-
+    } else {
+        println("List of the books is empty")
     }
 
 }
